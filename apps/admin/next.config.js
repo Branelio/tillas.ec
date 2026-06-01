938300ea262f4f2c7efa58b97ac7631cb93ddcb9
Branelio <bjsangoluisa@espe.edu.ec>
2026-06-01 12:42:35 -0500

    fix: deshabilitar optimizacion de imagenes para Minio y corregir wildcards nip.io

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'media.tillas.ec' },
       { protocol: 'http', hostname: 'localhost', port: '9000' },
-      { protocol: 'https', hostname: '*.nip.io' },
-      { protocol: 'https', hostname: 'media.*.nip.io' },
+      { protocol: 'https', hostname: '**.nip.io' },
+      { protocol: 'https', hostname: 'media.**.nip.io' },
     ],
   },
 };
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,11 +5,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'media.tillas.ec' },
       { protocol: 'http', hostname: 'localhost', port: '9000' },
-      { protocol: 'https', hostname: '*.nip.io' },
-      { protocol: 'https', hostname: 'media.*.nip.io' },
+      { protocol: 'https', hostname: '**.nip.io' },
+      { protocol: 'https', hostname: 'media.**.nip.io' },
     ],
   },
 };
